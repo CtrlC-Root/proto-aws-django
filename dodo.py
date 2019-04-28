b92df682af3d7ee1e429f8d11a6460baaa5d4a47
Alexandru Barbur <root.ctrlc@gmail.com>
2019-04-27 21:00:29 -0400

    Add initial task to build frontdesk PEX distribution.

--- dodo.py
+++ dodo.py
@@ -36,3 +36,31 @@ def task_update_web_ami():
             'value': ('detect_web_ami', 'ami')
         }
     }
+
+
+def task_build_frontdesk_pex():
+    package_root = os.path.join(os.getcwd(), 'pkg/frontdesk')
+    requirements_file = os.path.join(package_root, 'requirements.txt')
+    pex_file = os.path.join(package_root, 'frontdesk-0.1.pex')
+
+    return {
+        'file_dep': find_files(
+            package_root,
+            include=['*.py', '*.html'],
+            extra=['requirements.txt', 'MANIFEST.in']
+        ),
+        'actions': [
+            [
+                'pex',
+                '-v', package_root,
+                '-r', requirements_file,
+                '--disable-cache',
+                '-c', 'frontdesk',
+                '-o', pex_file
+            ],
+        ],
+        'targets': [
+            pex_file
+        ],
+        'clean': True
+    }
