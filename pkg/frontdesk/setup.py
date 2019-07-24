c54cc0645bb992c6411d09a92daf7ce95940363c
Alexandru Barbur <root.ctrlc@gmail.com>
2019-07-24 13:48:59 -0400

    Make a new release of the frontdesk app.

--- pkg/frontdesk/setup.py
+++ pkg/frontdesk/setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='frontdesk',
-    version='0.1',
+    version='0.2',
 
     description='A Django based package tracking application.',
     author='Alexandru Barbur',
